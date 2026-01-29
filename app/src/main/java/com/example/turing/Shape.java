66eeb0dc7738553c580488dd9ce6789efaa153d8
mirocsd <115913743+mirocsd@users.noreply.github.com>
2026-01-28 20:51:22 -0700

    Update Shape.java to resolve merge conflict

--- app/src/main/java/com/example/turing/Shape.java
+++ app/src/main/java/com/example/turing/Shape.java
@@ -3,11 +3,7 @@ package com.example.turing;
 public abstract class Shape {
     int x;
     int y;
-<<<<<<< HEAD
 
-    String color = "blue";
-=======
     String color = "not not yellow";
->>>>>>> 6b080164d1ba797f396ed4138f3bd9339758a209
 }
 
